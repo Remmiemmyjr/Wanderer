1bb11ed9645be42475cafed189f6420f3877473a
Remmiemmyjr <emily.berg@digipen.edu>
2020-01-10 17:25:32 -0800

    Made Enemy AI, doesnt work

---
+++ Wonderer/Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float moveSpeed = 4;
+    Vector3 velocity = new Vector3(0.0f, 0.0f, 0.0f);
+    Transform Player; 
+    int maxDistance = 15;
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Follow();
+    }
+
+    private void Follow()
+    {
+        if (Vector3.Distance(transform.position, Player.position) <= maxDistance)
+        {
+            transform.position += velocity * moveSpeed;
+        }
+        transform.position += velocity;
+    }
+}
