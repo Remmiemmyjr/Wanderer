327b0d0aa7c7c3eeef47480644ca7948436a2079
realGingerBreadMan <48932947+realGingerBreadMan@users.noreply.github.com>
2020-01-16 15:56:34 -0800

    fixes to level change

--- Wonderer/Assets/Scripts/LevelChange.cs
+++ Wonderer/Assets/Scripts/LevelChange.cs
@@ -8,13 +8,13 @@ public class LevelChange : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        int x = 5; 
+
     }
 
     void OnCollisionEnter2D(Collision2D col)
@@ -23,22 +23,7 @@ public class LevelChange : MonoBehaviour
         {
             SceneManager.LoadScene("Level2");
         }
-        
-    }
 
-    void OnCollisionStay2D(Collision2D col)
-    {
-        if (this.gameObject.tag.Equals("Player"))
-        {
-            SceneManager.LoadScene("Level2");
-        }
-
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "bullet")
-            Destroy(gameObject);
     }
 
 }
