03f35fff1e4bbfffebe9e43e5e89cfa2b6e631a8
realGingerBreadMan <48932947+realGingerBreadMan@users.noreply.github.com>
2020-01-17 17:33:00 -0800

    finished health script and added new scenes

--- Wonderer/Assets/Scripts/Health.cs
+++ Wonderer/Assets/Scripts/Health.cs
@@ -1,20 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
-    public static int healthBar = 3;
+    public static int healthCounter = 3;
     public static int Counter = 0;
     private static TextMesh crystalCount;
-    
+    private static TextMesh HealthBar;
+
     public bool DestroyOnCollide = true;
+    
     // Start is called before the first frame update
     void Start()
     {
         crystalCount = GameObject.Find("crystalCount").GetComponent<TextMesh>();
+        HealthBar = GameObject.Find("HealthBar").GetComponent<TextMesh>();
     }
-
+    
     // Update is called once per frame
     void Update()
     {
@@ -24,6 +28,23 @@ public class Health : MonoBehaviour
     {
         Counter += Collected;
         crystalCount.text = Counter.ToString();
+    }
+    public void UpdateHealthCounter(int healthChange)
+    {
+        if (DestroyOnCollide)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            healthCounter -= healthChange;
+            HealthBar.text = healthCounter.ToString();
+            //if health = 0 change scene to end scene
+            if(healthCounter == 0)
+            {
+                SceneManager.LoadScene("End");
+            }
+        }
     }
   /*  private void OnTriggerEnter2D(Collider2D other)
     {
